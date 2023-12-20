59c7368bfe89aa26b9e18d36067ef60043a52c6e
Kevin Hester <kevinh@geeksville.com>
2023-12-20 08:11:39 -0800

    fix feeder init if feeder loses power

--- petminion/Feeder.py
+++ petminion/Feeder.py
@@ -47,6 +47,11 @@ class ZigbeeFeeder(Feeder):
 
     def init_feeder(self):
         """Send MQTT to force feeder into manual mode, with no built-in scheduled feedings"""
+        # set portions_weight in attempt to fix error message seen on boot (if feeder lost power) - seems non fatal?
+        # Dec 20 07:57:41 petminion npm[595]: Zigbee2MQTT:info  2023-12-20 07:57:41: MQTT publish: topic 'zigbee2mqtt/feeder', payload '{"child_lock":"UNLOCK","error":false,"feed":"START","feeding_size":1,"feeding_source":"remote","led_indicator":"OFF","linkquality":163,"mode":"manual","portion_weight":8,"portions_per_day":0,"schedule":[],"serving_size":1,"weight_per_day":0}'
+        # Dec 20 07:57:41 petminion python[21390]: 07:57:41 DEBUG MQTT received zigbee2mqtt/feeder: b'{"child_lock":"UNLOCK","error":false,"feed":"START","feeding_size":1,"feeding_source":"remote","led_indicator":"OFF","linkquality":163,"mode":"manual","portion_weight":8,"portions_per_day":0,"schedule":[],"serving_size":1,"weight_per_day":0}'
+        # Dec 20 07:57:41 petminion npm[595]: Zigbee2MQTT:error 2023-12-20 07:57:41: Exception while calling fromZigbee converter: Expected one of: 1, 2, 4, 8, 16, 31, 32, 42, 64, 85, 96, 127, got: 'undefined'}
+
         # a full payload seems to contain '{"child_lock":"UNLOCK","error":false,"feed":"","feeding_size":1,"feeding_source":"schedule","led_indicator":"OFF","linkquality":149,"mode":"manual","portion_weight":8,"portions_per_day":7,"schedule":[],"serving_size":1,"weight_per_day":56}'
-        payload = '{"schedule":[]}'
+        payload = '{"schedule":[], "portion_weight":8, "portions_per_day":10, "weight_per_day":80 }'
         self.client.publish(command_topic, payload)
