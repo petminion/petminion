9a431d387a38a563d042ba3aba83f1cdb40477b3
geeksville <kevinh@geeksville.com>
2023-12-23 15:06:59 -0800

    autogen docs in gh-pages

---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+project = 'Petminion'
+copyright = '2023, Geeksville Industries, LLC.'
+author = 'Geeksville Industries, LLC.'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# our module src is one level up from here
+sys.path.insert(0, os.path.abspath(".."))
--- petminion/__init__.py
+++ petminion/__init__.py
@@ -1,4 +1,5 @@
 from .Trainer import Trainer
+from .TrainingRule import SimpleFeederRule, TrainingRule
 
 # The following exports are considered 'public' for this module
-__all__ = [Trainer]
+__all__ = ['Trainer', 'TrainingRule', 'SimpleFeederRule']
