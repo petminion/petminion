1cec468ff2d490c06f8c1fb5ba09ecc4c3e1569e
geeksville <kevinh@geeksville.com>
2023-12-29 07:49:53 -0800

    oops

--- petminion/SocialMediaClient.py
+++ petminion/SocialMediaClient.py
@@ -8,6 +8,6 @@ logger = logging.getLogger()
 class SocialMediaClient:
     """A client for posting images to social media platforms"""
 
-    def post_image(self, subreddit: str, title: str, image: numpy.ndarray) -> None:
+    def post_image(self, title: str, image: numpy.ndarray) -> None:
         """Given an image array, post that image to social media"""
         logger.warning("Social Media posts are disabled - not posting")
