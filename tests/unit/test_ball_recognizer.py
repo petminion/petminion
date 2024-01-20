d94005f9bce61787ab1e9b87e6290e573a0fc89c
geeksville <kevinh@geeksville.com>
2024-01-20 11:49:02 -0800

    add crude ball detector test

---
+++ tests/unit/test_ball_recognizer.py
@@ -0,0 +1,20 @@
+
+import os
+
+import cv2
+import pytest
+
+from petminion.BallRecognizer import BallRecognizer
+
+
+@pytest.fixture
+def recognizer():
+    return BallRecognizer()
+
+
+def test_detector(recognizer, test_image_dir) -> None:
+    # Read the image using OpenCV
+    path = os.path.join(test_image_dir, 'cat.jpg')
+    img = cv2.imread(path)
+    annotated, result = recognizer.do_detection(img)
+    assert result == []
