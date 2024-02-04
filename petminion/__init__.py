a6a21cc45272452bb3fdc0dc0abd81cfb095c702
geeksville <kevinh@geeksville.com>
2024-02-04 11:36:56 -0800

    temporarily make more public

--- petminion/__init__.py
+++ petminion/__init__.py
@@ -1,5 +1,6 @@
 from .Trainer import Trainer
 from .TrainingRule import SimpleFeederRule, TrainingRule
 
-# The following exports are considered 'public' for this module
-__all__ = ['Trainer', 'TrainingRule', 'SimpleFeederRule']
+# The following exports are considered 'public' for this module.  We don't define this because
+# we want the default rule of 'everything not starting with underscore is public' to apply.
+# __all__ = ['Trainer', 'TrainingRule', 'ScheduledTrainingRule', 'SimpleFeederRule']
